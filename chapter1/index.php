4f04ca51391b92870547c3dde199e306b766c83f
Damiano Bacci <damiano.bacci@gmail.com>
2024-03-05 12:55:01 +0100

    New collection of exercises

---
+++ chapter1/index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Damiano Bacci";
+echo "05035 Narni, Terni, Italy";
+echo "I am junior web developer looking forward to learn PHP.";
+
+?>
+
+<?php
+
+
+echo "<h1>This is a simple HTML page</h1>";
+echo "<p>The page has a title and a paragraph.</p>";
+
+
+?>
