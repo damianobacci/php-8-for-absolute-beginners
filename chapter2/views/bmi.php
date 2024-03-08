615c043bdcc3ab54410c6ca38fa0f99f498d0e29
Damiano Bacci <damiano.bacci@gmail.com>
2024-03-08 19:43:52 +0100

    BMI form

--- chapter2/views/bmi.php
+++ chapter2/views/bmi.php
@@ -4,6 +4,9 @@ $style = "
 nav a:nth-child(5) {
     text-decoration: underline;
 }
+.label {
+    display: block;
+}
 </style>";
 
 $quizIsSubmitted = isset($_POST["bmi-submitted"]);
@@ -16,14 +19,15 @@ if ($quizIsSubmitted) {
     $info = "
 <form method='post' action='index.php?page=bmi'>
 <p>Calculate your BMI with this tool.</p>
-<label for='weight'>Weight in KG</label><input type='number' name='weight' id='weight'/>
-<label for='height'>Height in CM</label><input type='number' name='height' id='height'/>
+<label class='label 'for='weight'>Weight in KG</label><input type='number' name='weight' id='weight'/>
+<label class='label for='height'>Height in CM</label><input type='number' name='height' id='height'/>
 <input type='submit' name='bmi-submitted' value='Calculate BMI'/>
 </form>";
 }
 
 function showQuizResponse( string $bmi) {
-    $response = "<p>Your BMI is $bmi</p>";
+    $formattedBMI = number_format($bmi,2);
+    $response = "<p>Your BMI is $formattedBMI</p><p>More precisely, it's $bmi</p>";
     $response .= "<p><a href='index.php?page=bmi'>Calculate again?</a></p>";
     return $response;
 }
--- chapter2/views/projects.php
+++ chapter2/views/projects.php
@@ -3,8 +3,8 @@ $info = "
 <h1>Projects I have worked on</h1>
 <p>Bunch of simple PHP projects</p>
 <ul>
-<li>An interactive quiz</li>
-<li>BMI Calculator</li>
+<li><a href='index.php?page=quiz'>An interactive quiz</a></li>
+<li><a href='index.php?page=bmi'>BMI Calculator</a></li>
 <li>Currency converter</li>
 </ul>";
 $style ="
